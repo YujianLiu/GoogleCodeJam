1679c201e9d5409d7e8fdbc0ebceeebfdb86ee73
YujianLiu <lyj19921022@gmail.com>
2017-04-06 20:19:17 -0400

    Initial Commit

---
+++ GoogleCodeJam/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GoogleCodeJam
+//
+//  Created by yujian liu on 4/6/17.
+//  Copyright © 2017 yujian liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
