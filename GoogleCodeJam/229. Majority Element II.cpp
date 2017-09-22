b6e87613f2018dc68170c95c08112da20cfc66c3
liuyujian <lyj19921022@gmail.com>
2017-09-22 11:53:41 -0400

    MacBook
    Boyer-Moore majority voting algorithm

--- GoogleCodeJam/229. Majority Element II.cpp
+++ GoogleCodeJam/229. Majority Element II.cpp
@@ -9,5 +9,24 @@
 #include "Header.h"
 
 vector<int> majorityElement(vector<int>& nums) {
+    int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
+    vector<int> result;
+    for(auto num : nums){
+        if (num == candidate1) {
+            count1++;
+        } else if(num == candidate2){
+            count2++;
+        }
+        else if (count1 == 0){
+            candidate1 = num;
+        }else if (count2 == 0){
+            candidate2 = num;
+        }
+        else{
+            count1--;
+            count2--;
+        }
+    }
+    
     
 }
